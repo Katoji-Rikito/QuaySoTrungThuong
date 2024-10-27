3b46d7777b6421aa35af7d168927e18c524f2626
Rikito Katoji <Hoangthinh99hp@gmail.com>
2024-10-27 18:02:54 +0700

    [UPDATE] Nút menu, footer

---
+++ resources/libs/app/js/configs.js
@@ -0,0 +1,31 @@
+/** @format */
+
+/** ========= CẤU HÌNH ========= */
+/**
+ * Hình nền
+ */
+const CFG_Background = "bg-main1.jpg";
+
+/**
+ * Năm hiện tại
+ */
+const CFG_CurrentYear = "2025";
+
+/**
+ * Danh sách nhân viên
+ */
+const CFG_Staffs = {
+	1: "Nguyễn Văn A",
+	2: "Nguyễn Văn B",
+	3: "Nguyễn Văn C",
+};
+
+/** ========= TIÊU ĐỀ ========= */
+/**
+ * Tên công ty
+ */
+const TITLE_NameCompany = "NameCompany";
+/**
+ * Chào mừng (ngắn)
+ */
+const TITLE_Welcome = "Welcome to my page!";
